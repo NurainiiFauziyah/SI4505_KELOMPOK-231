84f177042dbcd58a37f05baaa7ec561e99023986
hanifahcodes <hanifahrahmah08@gmail.com>
2024-05-05 22:29:41 +0700

    kamus penyakit 75%

---
+++ resources/views/kamuspenyakit/index.blade.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Farmedic - Kamus Penyakit</title>
+    <link rel="stylesheet" type="text/css" href="./about.css">
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Poppins|Inter|Passion One">
+</head>
+<body style="background-size: cover;">
+  <style>
+    html,body {
+      height: 100vh;
+      width: 100%;
+      background-color: whitesmoke;
+    }
+    .bgutama{
+    
+    background-size:cover;
+    background-position:center;
+    background-attachment: fixed;
+    text-align: center;
+    }
+  
+    .Bag4{
+      background: linear-gradient(180deg, #1B4445 0%, #43A9AB 100%); 
+      background-color: #1B4445;
+      background-position:center; 
+      background-size:cover;
+      background-attachment: fixed;
+      text-align: center;
+      border-bottom-right-radius:50px;
+      border-bottom-left-radius: 90px;
+      
+    }
+    a.RekomendasiObat{
+      font-family: Poppins;
+      font-size: 30px;
+      text-align: center;
+      float: right;
+      width: 700; height: 52px; 
+      left: 35px; top: 21px; position: absolute; 
+      text-align: center; color: black; font-size: 30px; font-weight: 600; letter-spacing: 2.40px; word-wrap: break-word;
+      
+    }
+    button.buttonrekomendasi {
+      font-family: Poppins;
+      font-size: 25px;
+      background: #838383;
+      color: white;
+      border: white 3px solid;
+      border-radius: 5px;
+      padding: 12px 20px;
+      margin-top: 10px;
+    }
+    .panduanperawatan {
+      background-color: white;
+    }
+    .container2 {
+      height: 0%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+    }
+  </style>
+  <div class="bghijau"></div>
+  <div class="bgutama" ></div>
+  
+    <nav class="Fitur" style="width: 918.05px; height: 50px; left: 449.95px; top: 34px; position: absolute">
+      <div class="Katalog" style="width: 54.50px; height: 14.15px; left: 0px; top: 19.09px; position: absolute">
+        
+      <div class="Forum" style="width: 44.69px; height: 11.07px; left: 120px; top: 19.09px; position: absolute">
+      
+      </div>
+      <div class="Artikel" style="width: 47.86px; height: 11.69px; left: 239.41px; top: 18.47px; position: absolute">
+     
+        </div>
+     
+      <div class="Ellipse219" style="width: 50px; height: 50px; left: 868.05px; top: 0px; position: absolute; background: #D9D9D9"></div>
+</nav>
+
+      <div class="Ellipse219" style="width: 50px; height: 50px; left: 868.05px; top: 0px; position: absolute; background: #D9D9D9"></div>
+    </div>
+  </div>
+ 
+  <div class="Rectangle2215" style="width: 1145px; height: 773px; left: 144px; top: 806px; position: absolute; background: #D9D9D9"></div>
+  <div class="Group2699" style="width: 1162.78px; height: 537px; left: 134px; top: 203px; position: absolute">
+    <div class="Rectangle2199" style="width: 1162.78px; height: 537px; left: 0px; top: 0px; position: absolute; background: #1F4D4E; border-radius: 46px"></div>
+    <div class="Rectangle14" style="width: 1145px; height: 508px; left: 10px; top: 18px; position: absolute; opacity: 0.50; background: #97AFAE; box-shadow: 20px 20px 20px; border-radius: 30px; filter: blur(20px)"></div>
+  </div>
+   
+ 
+  <img class="Logo" style="width: 162px; height: 36px; left: 30px; top: 67px; position: absolute" src="logo farmedic.png" />
+  
+      <img class="GambarPenyakit" src="https://th.bing.com/th/id/OIP.Kv6q8DFS1I3Npyv9ySRV5gAAAA?rs=1&pid=ImgDetMain" height="200px" width = "300px">
+      <div class="Frame1" style="padding: 10px"></div>
+    </div>
+  </div>
+ 
+       
+      <div class="Ellipse219" style="width: 50px; height: 50px; left: 868.05px; top: 0px; position: absolute; background: #D9D9D9"></div>
+    </div>
+  </div>
+  <div class="Bag4" style="width: 1580px; height: 619px; left: -20px; top: 0px; position: absolute">
+    
+    <nav class="container" style="width: 918.05px; height: 50px; left: 550px; top:34px; position: absolute; width:100vh">
+    <strong>
+    <a class="Katalog" href="katalog" style="width: 54.50px; height: 14.15px; left: 0px; top: 15px; position: absolute; font-family: Poppins; color: #D4D8DB; letter-spacing:1px">Katalog
+        
+    </a>
+      <a class="Forum" href="forum" style="width: 44.69px; height: 11.07px; left: 120px; top: 15px; position: absolute; font-family: Poppins; color:#D4D8DB; letter-spacing:1px">Forum
+        
+</a>
+      <a class="Artikel" href="artikel" style="width: 47.86px; height: 11.69px; left: 239.41px; top: 15px; position: absolute; font-family: Poppins; color:#D4D8DB; letter-spacing:1px">Artikel
+
+</a>
+      <a class="Treckmind" href="treckmind" style="width: 76.57px; height: 11.69px; left: 359.58px; top: 15px; position: absolute; font-family: Poppins; color:#D4D8DB; letter-spacing:1px">Treckmind
+
+</a>
+      <a class="Kontak" href="kontak" style="width: 50.53px; height: 11.07px; left: 504px; top: 15px; position: absolute; font-family: Poppins; color:#D4D8DB; letter-spacing:1px">Kontak
+
+</a>
+    </strong>
+     
+      <div class="Search" style="width: 131px; height: 34px; left: 620.05px; top: 8px; position: absolute">
+        <input type="text" placeholder="Search" class="Search" style="width: 131px; height: 34px; left: 5px; top: -2px; position: absolute; font-family: Poppins; color:#838383; border-radius: 30px; text-align: center">
+        </div>
+      </div>
+      <div class="profile" style="width: 50px; height: 50px; left: 1450px; top: 35px; position: absolute; background: #D9D9D9"></div>
+		<img src="http://0.gravatar.com/avatar/e45dd4168c39fa95cc0521956237a7e1" height="100px" width=auto>
+</nav>
+  </div>
+  </div>
+  <div class="Rectangle2216" style="width: 1450px; height: 1120px; left: 60px; top: 500px; background-position:center; position: absolute; margin:auto;background: white; box-shadow: 0px 2px 15px rgba(0, 0, 0, 0.25); border-radius: 41px"></div>
+  <div class="Rectangle2217" style="width: 1300px; height: 650px; left: 150px; top: 806px; position: absolute; background: #D9D9D9"></div>
+    <div class="PenjelasanPenyakitYangDiderita" style="width: 1200px; left: 200px; top: 830px; position: absolute; text-align: justify; color: black; font-size: 17px; font-family: Poppins; font-weight: 500; letter-spacing: 1px; word-wrap: break-word">
+        <p> Penyakit BEF (Bovine Ephemeral Fever) merupakan salah satu penyakit yang paling banyak diderita oleh ternak ruminansia, khususnya sapi. BEF sering muncul pada puncak musim kemarau atau saat pancaroba. Penyakit ini disebabkan oleh sebuah virus bernama Virus RNA. </p>
+          <p>Gejala yang kerap ditemukan pada penyakit BEF: <br/>
+              - Sapi tidak mampu berdiri <br/>
+              - Nafsu makan sapi berkurang <br/>
+              - Keluar lendir dari hidung sapi <br/>
+              - Kaki sapi pincang <br/> 
+          </p>
+          <p>
+            Diagnosa yang dilakukan pada penyakit BEF pada sapi ini didsarkan oleh gejala klinis, pemeriksaan patologis, uji serologis dan uji virologis dengan melakukan identifikasi virus.
+            Diagnosa dapat dilakukan dengan pengujian serologis, dengan deteksi <i>Complement Fixation Test</i> (CFT), <i>Serum Neutralization Test</i> (SNT), <i>Enzyme Linked Immunosorbent Assay</i> (ELISA) dan <i>Agar Gel Precipitation Test</i> (AGPT)
+          </p>
+          <p> Penularan penyakit BEF ini dapat melalui nyamuk <strong>nyamuk Cullicoides</strong> atau lalat. Apabila sapi terserang penyakit ini kemungkinan dapat menurunkan produksi daging dan susu.
+          Akan tetapi, tidak perlu khawatir karena tingkat kesembuhan dari penyakit BEF ini cukup tinggi dan angka kematiannya pun kurang dari 1%.
+          </p>
+          <p>
+            Penyakit BEF ini memiliki mortalitas yang rendah, namun tingkat penyebaran penyakit ini cukup tinggi. Oleh karena itu, perlu adanya tindakan pencegahan agar tidak menimbulkan kerugian bagi para peternak.
+            Pencegahan yang paling mudah dilakukan adalah dengan selalu menjaga keberdihan lingkungan, baik itu di lingkungan sekitar maupun lingkungan pada kandang hewan.
+          </p>
+        </div>
+  <div class="Group2700" style="width: 1162.78px; height: 537px; left: 120px; top: 203px; position: absolute">
+    <div class="Rectangle2199" style="width: 1345px; height: 537px; left: -5px; top: 0px; position: absolute; background: #1F4D4E; border-radius: 46px"></div>
+    <div class="Rectangle14" style="width: 1330px; height: 508px; left: 10px; top: 18px; position: absolute; opacity: 0.50; background: #97AFAE; box-shadow: 20px 20px 20px; border-radius: 30px; filter: blur(20px)"></div>
+  </div>
+  <div class="PanduanPerawatan" style="width: 300px; height: 94px; left: 47px; top: 1500px; position: absolute">
+    <div class="Rectangle2213" style="width: 500px; height: 70px; left: 180px; top: 0px; position: absolute; background: #D4D8DB; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 31px"></div>
+    <nav class="PanduanPerawatan" style="width: 290px; height: 45px; left: 290px; top: 20px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Poppins; font-weight: 600; letter-spacing: 2.40px; word-wrap: break-word">
+      <a class="pandu" href="panduanperawatan" style>Panduan Perawatan</a>
+  </nav>
+
+  </div>
+  <div class="RekomendasiObat" href="rekomendasi obat" style="width: 500px; height: 70px; left: 890px; top: 1500px; position: absolute;  background: #D4D8DB; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 31px">
+    <nav class="RekomendasiObat" style="width: 300px; height: 35px; left: 110px; top: 20px; position: absolute; text-align: center; color: black; font-size: 20px; font-family: Poppins; font-weight: 600; letter-spacing: 2.40px; word-wrap: break-word;">
+      <a class="rekomendasi" href="rekomendasiobatt">Rekomendasi Obat</a>
+  </nav>
+  </div>
+  <h1 style="left: 610px; top: 210px; position: absolute; text-align: center; color: white; font-size: 40px; font-family: Poppins; font-weight:900; letter-spacing: 2px; word-wrap:normal">KAMUS PENYAKIT</h1>
+    <h2 style="left: 588px; top: 625px; position: absolute; text-align: center; color: white; font-size: 30px; font-family: Poppins; font-weight: 400; letter-spacing: 2.2px; word-wrap: break-word">Bovine Enhemeral Fever</h2>
+ 
+  <img class="Logo" style="width: 162px; height: 36px; left: 30px; top: 55px; position: absolute" src="logo farmedic.png" alt="FARMEDIC LOGO" />
+  <div class="container2" style="position: absolute; top: 57%; left: 37.5%; text-align:center; transform: translateY(-50%);">
+    <img class="GambarPenyakit" src="https://th.bing.com/th/id/OIP.Kv6q8DFS1I3Npyv9ySRV5gAAAA?rs=1&pid=ImgDetMain" style="width: 410px; border-radius: 19px"/>
+    </div>
+  </div>
+</div>
+</body>
+
+
