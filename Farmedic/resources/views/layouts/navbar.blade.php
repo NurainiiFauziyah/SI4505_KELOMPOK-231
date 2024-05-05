0f95e0a335f537181356a879740b4c47d1c724f0
arkanzafran <91584760+arkanzafran@users.noreply.github.com>
2024-05-05 21:58:23 +0700

    kelar

--- Farmedic/resources/views/layouts/navbar.blade.php
+++ Farmedic/resources/views/layouts/navbar.blade.php
@@ -7,8 +7,8 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup" style="color: #FFFFFF">
             <div class="nav-item dropdown" style="padding: 0 15px;">
-                <a class="nav-link" href="#" id="navbarDropdownLayanan" role="button" style="color: #FFFFFF;">
-                    Layanan
+                <a class="nav-link d-flex align-items-center" href="#" id="navbarDropdownLayanan" role="button" style="color: #FFFFFF;">
+                    Layanan <img src="https://cdn-icons-png.flaticon.com/512/25/25243.png" alt="dropdown icon" style="width: 12px; height: 12px; margin-left: 5px; filter: brightness(0) invert(1);">
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdownLayanan" style="background-color: #FFFFFF; border-radius: 8px; padding: 10px; box-shadow: 0 4px 8px rgba(0,0,0,0.1); display: none; transition: opacity 0.5s ease;">
                     <a class="dropdown-item" href="#" style="color: #1B4445;">Kamus penyakit</a>
