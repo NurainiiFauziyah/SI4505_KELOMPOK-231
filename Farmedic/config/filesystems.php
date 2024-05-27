9847eb6cee699869a327e09a756bcbd8144aacf7
nfrajabi <nfrajabi7@gmail.com>
2024-05-28 06:55:21 +0700

    K231-13 - Belum Connect dengan database "Login"

--- Farmedic/config/filesystems.php
+++ Farmedic/config/filesystems.php
@@ -33,7 +33,7 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
-            'throw' => false,
+           
         ],
 
         'public' => [
@@ -72,5 +72,6 @@ return [
     'links' => [
         public_path('storage') => storage_path('app/public'),
     ],
+
 
 ];
--- Farmedic/resources/views/Konsultasi/index.blade.php
+++ Farmedic/resources/views/Konsultasi/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.AppKonsultasi')
+@extends('layouts.app')
 @extends('layouts.layout')
 
 @section('content')
